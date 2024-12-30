236dd9a8498fc76cb0a491ef490550b192930bbc
Vishal-pandey17 <vishalpandey86681@gmail.com>
2024-12-30 13:11:43 +0530

    Navbar section completed successfullys

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -11,7 +11,7 @@ function App() {
        <StyledEngineProvider injectFirst> 
          <Navbar/>
          {/* <Outlet /> */}
-         <Hero/>
+         <Hero/>   
        </StyledEngineProvider>
     </>
   );
